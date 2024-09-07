48447ce1fa61f516ced45e1d440195da1e766c4e
Benjamin Tsang <54254486+tsangh5@users.noreply.github.com>
2024-09-07 12:04:00 -0400

    fix breakpoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@
 	// Breakpoints.
 		breakpoints({
 			xlarge:  [ '1281px',  '1800px' ],
-			large:   [ '981px',   '1280px' ],
+			large:   [ '1277px',   '1280px' ],
 			medium:  [ '737px',   '980px'  ],
 			small:   [ '481px',   '736px'  ],
 			xsmall:  [ null,      '480px'  ],
